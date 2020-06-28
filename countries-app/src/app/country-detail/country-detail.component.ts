7aa89de019214185dfe94b0fa86e9e738e0299ed
Gianinna Caceres <gianinna.patricia@gmail.com>
2020-06-27 20:09:05 -0500

    :art: Fix styles and set themes

--- countries-app/src/app/country-detail/country-detail.component.ts
+++ countries-app/src/app/country-detail/country-detail.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Country } from '../models/Country';
 import { CountriesService } from '../countries.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
   styleUrls: ['./country-detail.component.less']
 })
-export class CountryDetailComponent implements OnInit {
-  country: Country;
+export class CountryDetailComponent implements OnInit, OnDestroy {
+  private destroyed$ = new Subject();
 
-  constructor(private route: ActivatedRoute, 
+  country: Country;
+  
+  constructor(
+    private route: ActivatedRoute, 
     private service: CountriesService) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(country => {
+    this.route.params
+    .pipe(takeUntil(this.destroyed$))
+    .subscribe(country => {
       if(country && country.id) { 
         this.loadDetails(country.id);
       }
@@ -24,7 +31,12 @@ export class CountryDetailComponent implements OnInit {
 
   loadDetails(countryName: string) {
     this.service.getCountryByName(countryName)
+    .pipe(takeUntil(this.destroyed$))
     .subscribe(countryDetails => this.country = countryDetails);
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
   }
 
 }
